a0fd287ffa40acd585366e3a2eb0c2506d5a1536
hello-doly <anylocalwork@gmail.com>
2026-06-27 11:55:12 +0530

    Add new js file

---
+++ text.js
@@ -0,0 +1,11 @@
+// console.log("Aditya kumar")
+
+// let arr =[1,2,3,4,5];
+
+// let sum = arr.reduce((acc, curr) => acc +curr)
+
+// console.log(sum)
+
+let str = 'Aditya kumar maurya knowkedge '
+
+let longest = str.split('').reduce()
